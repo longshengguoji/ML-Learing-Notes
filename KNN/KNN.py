99bd321a73a4b2e1f2061319a4e1cd8f5f2e039d
zhuwei <lsgjzhuwei@163.com>
2016-12-08 23:49:04 +0800

    增加数据归一化函数

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -47,3 +47,13 @@ def file2matrix(filename):
             classLabelVector.append(love_dictionary.get(listFromLine[-1]))
         index += 1
     return returnMat,classLabelVector
+    
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
+    return normDataSet, ranges, minVals
